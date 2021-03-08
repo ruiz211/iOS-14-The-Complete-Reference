2490019a0395d5de73567d68a5991511624ac5d5
ruiz211 <ruiz.im211@gmail.com>
2021-03-08 16:57:47 -0500

    Hello world

---
+++ Projects/1/helloworld/Hello World/Shared/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Shared
+//
+//  Created by José Ruiz on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
