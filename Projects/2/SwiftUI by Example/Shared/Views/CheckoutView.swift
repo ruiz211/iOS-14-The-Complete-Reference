23c9f138ecadfae7ee4c7584b78787cafd790f10
ruiz211 <ruiz.im211@gmail.com>
2021-03-11 16:34:28 -0500

    Presenting an alert

--- Projects/2/SwiftUI by Example/Shared/Views/CheckoutView.swift
+++ Projects/2/SwiftUI by Example/Shared/Views/CheckoutView.swift
@@ -11,11 +11,10 @@ struct CheckoutView: View {
     @EnvironmentObject var order: Order
     
     @State private var payment: String = "Cash"
-    
     @State private var addLoyaltyDetails: Bool = false
     @State private var loyaltyNumber = ""
-    
     @State private var tip: Int = 5
+    @State private var showConfirmationAlert = false
     
     let payments: [String] = ["Cash", "Credit Card", "iDine Points"]
     let tips: [Int] = [0, 2, 3, 5, 7, 11 ,13]
@@ -56,11 +55,14 @@ struct CheckoutView: View {
             
             Section(header: Text("Total: \(totalAmount)")) {
                 Button("Confirm order") {
-                    
+                    showConfirmationAlert.toggle()
                 }
             }
         }
         .navigationTitle("Checkout").navigationBarTitleDisplayMode(.inline)
+        .alert(isPresented: $showConfirmationAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was: \(totalAmount)"), dismissButton: .default(Text("Done")))
+        }
     }
 }
 
